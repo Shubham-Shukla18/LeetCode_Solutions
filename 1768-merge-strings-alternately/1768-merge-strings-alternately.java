026eff53649a819bda74daa861656cdcc4cac36a
Shubham-Shukla18 <shubham246899@gmail.com>
2026-01-23 13:03:19 +0530

    Time: 0 ms (100%), Space: 43.2 MB (53.75%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -1,38 +1,64 @@
-import java.lang.Math;
+//import java.lang.Math;
 
 class Solution {
 
-    public static boolean isInvalidString(String word) {
-        return word==null || word.trim().isEmpty();
-    }
+    // My First approach time complexity is 4ms
+    // public static boolean isInvalidString(String word) {
+    //     return word==null || word.trim().isEmpty();
+    // }
+
+    // public String mergeAlternately(String word1, String word2) {
+    //     if(isInvalidString(word1) && isInvalidString(word2)) {
+    //         return "";
+    //     }
+
+    //     if(isInvalidString(word1)) {
+    //         return word2;
+    //     }
+
+    //     if(isInvalidString(word2)) {
+    //         return word1;
+    //     }
+
+    //     int word1_len = word1.length();
+    //     int word2_len = word2.length();
+    //     int max_len = Math.max(word1_len, word2_len);
+    //     String result_word = "";
+
+    //     for(int i=0; i<max_len; i++) {
+    //         if(i<word1_len) {
+    //             result_word +=word1.charAt(i);
+    //         }
+    //         if(i<word2_len) {
+    //             result_word +=word2.charAt(i);
+    //         }
+    //     }
+
+    //     return result_word;
+    // }
+
+    // new approach
 
     public String mergeAlternately(String word1, String word2) {
-        if(isInvalidString(word1) && isInvalidString(word2)) {
-            return "";
-        }
+    StringBuilder result = new StringBuilder();
 
-        if(isInvalidString(word1)) {
-            return word2;
-        }
+    int i=0;
+    int len1 = word1.length();
+    int len2 = word2.length();
 
-        if(isInvalidString(word2)) {
-            return word1;
-        }
-
-        int word1_len = word1.length();
-        int word2_len = word2.length();
-        int max_len = Math.max(word1_len, word2_len);
-        String result_word = "";
-
-        for(int i=0; i<max_len; i++) {
-            if(i<word1_len) {
-                result_word +=word1.charAt(i);
-            }
-            if(i<word2_len) {
-                result_word +=word2.charAt(i);
-            }
-        }
-
-        return result_word;
+    for(; i<len1 && i<len2; i++) {
+        result.append(word1.charAt(i));
+        result.append(word2.charAt(i));
     }
+
+    if(i<len1) {
+        result.append(word1.substring(i));
+    }
+    if(i<len2){
+        result.append(word2.substring(i));
+    }
+
+    return result.toString();
+    }
+
 }
