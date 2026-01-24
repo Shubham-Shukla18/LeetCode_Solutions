0e1c3d12937839f401df4f7bb942ec28db02d632
Shubham-Shukla18 <shubham246899@gmail.com>
2026-01-24 09:14:07 +0530

    Time: 1 ms (90.46%), Space: 43.8 MB (18.73%) - LeetHub

--- 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
+++ 1071-greatest-common-divisor-of-strings/1071-greatest-common-divisor-of-strings.java
@@ -18,4 +18,18 @@ class Solution {
         }
         return a;
     }
+
+    // recursive call
+    //  public String gcdOfStrings(String str1, String str2) {
+    //     if (str2.length() > str1.length()) {
+    //         return gcdOfStrings(str2, str1);
+    //     }
+    //     if (str2.equals(str1)) {
+    //         return str1;
+    //     }
+    //     if (str1.startsWith(str2)) {
+    //         return gcdOfStrings(str1.substring(str2.length()), str2);
+    //     }
+    //     return "";
+    // }
 }
