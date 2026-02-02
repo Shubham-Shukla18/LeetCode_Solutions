5ee8fd604ca5c1914e572b01e5def3bdc59bebec
Shubham-Shukla18 <shubham246899@gmail.com>
2026-02-02 12:24:30 +0530

    Time: 592 ms (89.09%), Space: 0B (100%) - LeetHub

---
+++ 1731-the-number-of-employees-which-report-to-each-employee/1731-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,6 @@
+/* Write your PL/SQL query statement below */
+select distinct e2.employee_id, e2.name, count(*) reports_count, round(avg(e1.age)) average_age
+from Employees e1
+join Employees e2 on e1.reports_to = e2.employee_id
+group by e2.employee_id, e2.name
+order by e2.employee_id;
