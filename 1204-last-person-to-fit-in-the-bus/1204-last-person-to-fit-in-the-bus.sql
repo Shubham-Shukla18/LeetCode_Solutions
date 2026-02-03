ffa6c4abca68b395cc2240dbd17830f153f9049c
Shubham-Shukla18 <shubham246899@gmail.com>
2026-02-03 12:35:00 +0530

    Time: 662 ms (57.04%), Space: 0B (100%) - LeetHub

--- 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
+++ 1204-last-person-to-fit-in-the-bus/1204-last-person-to-fit-in-the-bus.sql
@@ -1,4 +1,14 @@
 /* Write your PL/SQL query statement below */
+
+/*
+Approach=> using keep 
+TC=> O(n)
+SC=> O(1)
+Speed=> fastest
+object creation => none
+memory pressure => low
+Application => reporting/dashboards
+*/
 select max(person_name) keep (dense_rank last order by cumulative_sum) person_name
 from (
     select person_name,
@@ -7,4 +17,3 @@ from (
     Queue
 ) 
 where cumulative_sum<=1000
-
