a186a80174d6952cd7b64c8fd7ba4c02d4a23947
Shubham-Shukla18 <shubham246899@gmail.com>
2026-03-04 06:20:46 +0530

    Update 0394-decode-string.java
    update the method comment

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -12,6 +12,15 @@ class Solution {
         }));
     }
 
+    /*
+    Approach=> recursive approach
+    TC=> O(L) (where L is result length)
+    SC=> O(D+N) (depth + input)
+    Speed=> fast(native stack optimization)
+    object creation => minimal (Queue + StringBuilder)
+    memory pressure => low
+    Application => Grammer Parsers / Compilers
+    */    
     private String decode(Queue<Character> queue) {
         StringBuilder sb = new StringBuilder();
         int num = 0;
@@ -48,34 +57,3 @@ class Solution {
     }
 }
 
-// Another Solution:
-// class Solution {
-//     int n;
-
-//     public String decodeString(String s) {
-//         n = s.length() + 2;
-//         return decodeString("[".concat(s).concat("]"), new StringBuilder(), new int[] { 0 });
-//     }
-
-//     private String decodeString(String s, StringBuilder sb, int[] i) {
-//         boolean start = false;
-//         int rep = 0;
-//         while (i[0] < n) {
-//             char c = s.charAt(i[0]);
-//             if (Character.isDigit(c) && !start) {
-//                 rep *= 10;
-//                 rep += c - '0';
-//             } else if (Character.isDigit(c) && start) {
-//                 sb.append(decodeString(s, new StringBuilder(), i));
-//             } else if (c == '[' && !start) {
-//                 start = true;
-//             } else if (c == ']' && start) {
-//                 break;
-//             } else {
-//                 sb.append(c);
-//             }
-//             i[0]++;
-//         }
-//         return sb.toString().repeat(rep == 0 ? 1 : rep);
-//     }
-// }
